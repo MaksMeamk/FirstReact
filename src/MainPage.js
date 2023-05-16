d105ae96ad4fccf2a98dbe7475ca59cff20175b4
MaksMemakWork <maks_memak@mail.ru>
2023-05-16 13:39:44 +0000

    useLocation useNavigate

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,16 +1,24 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 const MainPage = () => {
+  const location = useLocation();
+  useEffect(() => {
+    console.log("My path:", location.pathname);
+  }, [location]);
+  const navigate = useNavigate();
   return (
     <>
       <nav>
         <ul>
           <li>
-            <Link to="/one">Page One</Link>
+            <Link to="/">Page Main</Link>
           </li>
           <li>
-            <Link to="/two">Page Two</Link>
+            <button onClick={() => navigate("one")}>Page One</button>
+          </li>
+          <li>
+            <button onClick={() => navigate("two")}>Page Two</button>
           </li>
         </ul>
       </nav>
